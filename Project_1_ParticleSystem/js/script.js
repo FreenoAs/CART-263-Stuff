52e3a0a71f60aa831f3585672ffb329269fa787c
FreenoAs <awsomegamer2081@gmail.com>
2023-01-26 16:39:52 -0500

    just making sure i dont randomly lose this

--- Project_1_ParticleSystem/js/script.js
+++ Project_1_ParticleSystem/js/script.js
@@ -6,28 +6,68 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-"use strict";
-
 
 /**
 Description of preload
 */
-function preload() {
+let ball = []; // Declare array
 
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
-
+    createCanvas(710, 400);
+    for(let i = 0; i < 50; i++){
+        ball[i] = new Particle();
+    }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+    background(0);
+    for(let i = 0; i < ball.length; i++){
+        ball[i].move();
+        ball[i].display();
+        ball[i].follow();
+    } 
+} 
 
+/*function mousePressed() {
+    for (let i = 0; i < ball.length; i++) {
+            ball.splice(i, 1);
+    }
+}*/
+
+function keyPressed() {
+    ball.push(new Particle());
+}
+
+class Particle {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.speed = 100;
+        this.diameter = random(50);
+    }
+    move() {
+        this.x += random(mouseX) / 5;
+        this.y += random(mouseY) / 5;
+        // if (mouseX == width / 2 && mouseY == height / 2) {
+        //     this.speed = 5;
+        // 
+    
+    }
+    display() {
+            noStroke();
+            fill(this.x, this.y, this.diameter);
+            ellipse(this.x, this.y, this.diameter);
+    }
+    follow() {
+        this.x = mouseX;
+        this.y = mouseY;
+    }
+}
+
+class fallingStars {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+    } display() {
+    }
 }
