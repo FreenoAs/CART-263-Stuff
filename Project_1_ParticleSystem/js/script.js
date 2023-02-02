d633f0d40bbd3c76d1c3cf07f1f69b1b24b1b124
FreenoAs <awsomegamer2081@gmail.com>
2023-02-01 21:31:20 -0500

    got the particle to change depending where he is :D

--- Project_1_ParticleSystem/js/script.js
+++ Project_1_ParticleSystem/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Exploration and curiosity
+Anthony Scopacasa
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -11,60 +11,86 @@ author, and this description to match your project!
 Description of preload
 */
 let ball = []; // Declare array
+let red = 0;//color controllers
+let blue = 255;
+let green = 255;
 
-function setup() {
+function setup() {//creates the initial particle and canvas
     createCanvas(710, 400);
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 2; i++){
         ball[i] = new Particle();
     }
 }
 
 function draw() {
-    background(0);
-    for(let i = 0; i < ball.length; i++){
+    background(200);
+    for (let i = 0; i < ball.length; i++) {//allows the fabrication of the ball
         ball[i].move();
         ball[i].display();
         ball[i].follow();
-    } 
-} 
-
-/*function mousePressed() {
-    for (let i = 0; i < ball.length; i++) {
-            ball.splice(i, 1);
+        if (mouseX <= width / 4 && mouseY <= height / 4) {//makes the ball freak out and red in the top left
+            ball[i].diameter = 100;
+            ball[i].speed = 100;
+            red = 255;
+            blue = 0;
+        } else {//reverts it to normal
+            ball[i].diameter = 20;
+            ball[i].speed = 5;
+            blue = 255;
+            red = 0;
+        }if (mouseX >= width / 2 && mouseY <= height / 4) {//makes the ball green and nervous in the top right(add sweat drops)
+            blue = 0;
+            green = 255;
+        } else {
+            green = 0;
+        }if (mouseX >= width / 1.25 && mouseY >= height / 4) {//smol little guy
+            ball[i].diameter = 5;
+            ball[i].speed = 10;
+            blue = 255;
+        } if (mouseX <= width / 4 && mouseY >= height / 8) {
+            ball[i].x = mouseX + 100;
+        } if (mouseX <= width / 1.25 && mouseX >width / 3.5 && mouseY >= height / 1.5) {
+            red = 255;
+            blue = 255;
+        }
     }
-}*/
-
-function keyPressed() {
-    ball.push(new Particle());
 }
 
-class Particle {
-    constructor() {
-        this.x = random(width);
-        this.y = random(height);
-        this.speed = 100;
-        this.diameter = random(50);
-    }
-    move() {
-        this.x += random(mouseX) / 5;
-        this.y += random(mouseY) / 5;
-        // if (mouseX == width / 2 && mouseY == height / 2) {
-        //     this.speed = 5;
-        // 
+/*function panic() {
+   
+}
+
+function nervous() {
+   
+}
+
+function smol(){
     
+}*/
+
+class Particle {
+    constructor() {//makes the ball and controls speed and size
+        this.x = mouseX;
+        this.y = mouseY;
+        this.speed = 2;
+        this.diameter = 20;
     }
-    display() {
-            noStroke();
-            fill(this.x, this.y, this.diameter);
-            ellipse(this.x, this.y, this.diameter);
+    move() {//makes it jittery
+        this.x += random(-this.speed, this.speed);
+        this.y += random(-this.speed, this.speed);
     }
-    follow() {
+    display() {//displays the little guy
+        noStroke();
+        fill(red,green,blue);
+        ellipse(this.x, this.y, this.diameter);
+    }
+    follow() { //makes the lil guy follow the mouse
         this.x = mouseX;
         this.y = mouseY;
     }
 }
 
-class fallingStars {
+class fallingStars {//i want this to be sweat droplets
     constructor() {
         this.x = random(width);
         this.y = random(height);
