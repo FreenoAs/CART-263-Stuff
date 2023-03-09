8db4946b95ca8704851c2143cbebc513c9e83fab
FreenoAs <awsomegamer2081@gmail.com>
2023-03-09 00:04:08 -0500

    got random hero to work (experimenting on a hero search too might simplify it)

--- data visualization/js/script.js
+++ data visualization/js/script.js
@@ -12,6 +12,9 @@ author, and this description to match your project!
 let table;
 let points = [];
 let button;
+let buttonSummon;
+let buttonSearch;
+let inputSearch;
 
 function preload() {
     table = loadTable('fire emblem heroes.csv', 'csv', 'header');
@@ -20,8 +23,6 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
-    console.log(table.getRowCount() + " total rows in table");
-    console.log(table.getColumnCount() + " total columns in table");
     let rows = table.findRow('Heroes', 'Game');
     for (var r = 0; r < table.getRowCount(); r++) { // Cycle through each row of the table
             points[r] = new DataPoint(table.getString(r, 0),
@@ -45,7 +46,7 @@ function setup() {
 
 function heroSummonerButton() {//bring up the hero summoner screen
     button = createButton("Hero Summoner");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(drawHeroSummoner,deleteButton);
+    button.mouseClicked(drawHeroSummoner);
     button.size(100, 25);
     button.position(width / 1.54, height / 1.1);
     button.style("font-size", "10px");
@@ -54,7 +55,7 @@ function heroSummonerButton() {//bring up the hero summoner screen
 
 function movementTypeButton() {//changes every characters color to their corresponding movement type in the top left
     button = createButton("Movement Type");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(drawMovementType,deleteButton);
+    button.mouseClicked(drawMovementType);
     button.size(100, 25);
     button.position(width / 2.001, height / 1.1);
     button.style("font-size", "10px");
@@ -63,7 +64,7 @@ function movementTypeButton() {//changes every characters color to their corresp
 
 function heroesButton() {//returns to the default screen with all the hero information
     button = createButton("All Heroes");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(drawHeroes,deleteButton);
+    button.mouseClicked(drawHeroes);
     button.size(100, 25);
     button.position(width / 20, height / 1.1);
     button.style("font-size", "10px");
@@ -71,8 +72,8 @@ function heroesButton() {//returns to the default screen with all the hero infor
 }
 
 function mostAltsButton() {//displays the units with the most hero variations
-    button = createButton("Most Alts");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(mostAlts,deleteButton);
+    button = createButton("Hero Search");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
+    button.mouseClicked(heroSearch);
     button.size(100, 25);
     button.position(width / 5, height / 1.1);
     button.style("font-size", "10px");
@@ -81,7 +82,7 @@ function mostAltsButton() {//displays the units with the most hero variations
 
 function genderDifferenceButton() {//color codes the heroes to their gender
     button = createButton("Gender Difference");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(genderDifference,deleteButton);
+    button.mouseClicked(genderDifference);
     button.size(100, 25);
     button.position(width / 2.85, height / 1.1);
     button.style("font-size", "9px");
@@ -89,26 +90,39 @@ function genderDifferenceButton() {//color codes the heroes to their gender
 }
 
 function drawHeroSummoner() {//brings up the hero summon button on the hero summoning screen
-    button = createButton("Summon");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
-    button.mouseClicked(summonHero);
-    button.size(100, 25);
-    button.position(width / 2.25, height / 2);
-    button.style("font-size", "10px");
+    buttonSummon = createButton("Summon");// button functionailty sourced from here https://editor.p5js.org/dwantilus/sketches/H1MzBzBT-
+    buttonSummon.mouseClicked(summonHero);
+    buttonSummon.size(100, 25);
+    buttonSummon.position(width / 2.25, height / 2);
+    buttonSummon.style("font-size", "10px");
     background(0);
+    deleteSearchButton();
 }
 
 function summonHero() { //generates a random hero
+    background(0);
     stroke(10);
     textSize(20);
-    for (var r = 0; r < table.getRowCount(); r++) {
-        for (var c = 0; c < table.getColumnCount(); c++) {
-            table.getString(random(922));
-        }
-    }
+    fill(255);
+    points[0].summonHero(table.getRow(Math.floor(Math.random()*922)));//sourced from https://stackoverflow.com/questions/48038491/generate-a-random-integer-in-p5-js
 }
 
 function deleteButton() {
-    button.hide(drawHeroSummoner);
+    if (buttonSummon) {
+        buttonSummon.hide();
+    }
+}
+
+function deleteSearchButton() {
+    if (buttonSearch) {
+        buttonSearch.hide();
+    }
+}
+
+function deleteInput() {
+    if (inputSearch) {
+        inputSearch.hide();
+    }
 }
 
 function drawMovementType() {//changes unit color based on movement type
@@ -127,6 +141,12 @@ function drawMovementType() {//changes unit color based on movement type
     text("Flier", 50, 70);
     fill(255, 230, 90);
     text("Armoured", 50, 80);
+    fill(150);
+    textSize(10);
+    text("Alternate Variation Below Character", 50, 90);
+    deleteButton();
+    deleteInput();
+    deleteSearchButton();
 }
 
 function drawHeroes() {//displays all of the heroes
@@ -136,6 +156,13 @@ function drawHeroes() {//displays all of the heroes
             points[r].drawHeroes();
         }
     }
+    stroke(10);
+    textSize(15);
+    fill(255);
+    text("Game Below Character", 50, 50);
+    deleteButton();
+    deleteInput();
+    deleteSearchButton();
 }
 
 function genderDifference() {//seperates all the heroes based on gender
@@ -152,17 +179,26 @@ function genderDifference() {//seperates all the heroes based on gender
     text("Male", 50, 60);
     fill(150);
     text("Non-Binary", 50, 70);
-
+    deleteButton();
+    deleteInput();
+    deleteSearchButton();
 }
 
-function mostAlts() {//displays the units with the most alternate variations (most rows)
+function generateHero() {
+    let rows = table.findRows(inputSearch.value(''), 'Name');
+        fill(random(255), random(255), random(255));
+    text("Lyn", DataPoint.x, DataPoint.y);
+    console.log(inputSearch.value(''));
+}
+
+function heroSearch() {//displays the units with the most alternate variations (most rows)
     background(0);
-    for (var r = 0; r < table.getRowCount(); r++) {
-        for (var c = 0; c < table.getColumnCount(); c++) {
-            points[r].drawMostAlts();
-            points[c].drawMostAlts();
-        }
-    }
+    inputSearch = createInput();//input referenced from https://p5js.org/examples/dom-input-and-button.html
+    inputSearch.position(width / 2, height / 2);
+    buttonSearch = createButton('Search');
+    buttonSearch.position(width / 2.4, height / 2);
+    buttonSearch.mousePressed(generateHero);
+    deleteButton();
 }
 
 class DataPoint {//creates the heroes object
@@ -179,94 +215,118 @@ class DataPoint {//creates the heroes object
         this.y = random (height);
     }
     drawHeroes() {//displays all heroes by which game they originate from
-        noStroke();
+        stroke(5);
         textSize(5);
         if (this.Game == "Heroes") {
             fill(225, 255, 79);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Mystery") {
             fill(25, 30, 220);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Valentia") {
             fill(35, 230, 100);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Geneology") {
             fill(220, 225, 145);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Thracia") {
             fill(255, 0, 0);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Binding") {
             fill(255, 0, 0);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Blazing") {
             fill(120, 235, 140);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Sacred Stones") {
             fill(150, 240, 235);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Path of Radiance") {
             fill(90, 160, 200);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Radiant Dawn") {
             fill(25, 75, 100);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Awakening") {
             fill(0, 135, 255);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Fates") {
             fill(255);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Three Houses") {
             fill(235, 235, 150);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Tokyo Mirage") {
             fill(255, 55, 210);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         } if (this.Game == "Engage") {
             fill(0, 0, 255);
             text(this.Name, this.x, this.y);
+            text(this.Game, this.x, this.y + 5);
         }
     }
     drawGender() {//color codes all heroes by gender
         if (this.Gender == "Female") {
+            let rows = table.findRows('Female', 'Gender');
             textSize(5);
             fill(255, 150, 220);
-            text(this.Name, this.x, this.y);
+            ellipse(this.x, this.y, rows.length/20);
         } else if (this.Gender == "Male") {
+            let rows = table.findRows('Male', 'Gender');
             fill(0, 0, 255);
-            text(this.Name, this.x, this.y);
+            ellipse(this.x, this.y, rows.length/20);
         } else if (this.Gender == "Non-Binary") {
+            let rows = table.findRows('Non-Binary', 'Gender');
             fill(150);
-            text(this.Name, this.x, this.y);
+            ellipse(this.x, this.y, rows.length*30);
         }
     }
     drawMostAlts() {//increases the font size of the characters with the most amount of alternate variations (rows) 
-        if (table.getRowCount("Lyn") >= 3) {
-            textSize(20);
-            text(this.Name, this.x, this.y);
-        } else {
-            textSize(5);
-            text(this.Name, this.x, this.y);
-        }
+        fill(random(255), random(255), random(255));
+        //text(, this.x, this.y);
     }
     drawMovementType() {//color codes units based on their movement type
         if (this.MovementType == "Infantry") {
-            textSize(5)
+            textSize(7);
             fill(255, 55, 0);
+            square(this.x, this.y - 10, 25);
             text(this.Name, this.x, this.y);
+            text(this.AlternateVariation, this.x, this.y + 5);
         } if (this.MovementType == "Cavalry") {
-            textSize(5)
+            textSize(7);
             fill(0, 230, 255);
+            square(this.x, this.y - 10, 25, 5, 10);
             text(this.Name, this.x, this.y);
+            text(this.AlternateVariation, this.x, this.y + 5);
         } if (this.MovementType == "Flier") {
-            textSize(5)
+            textSize(7);
             fill(0, 255, 205);
+            ellipse(this.x + 7, this.y, 35);
             text(this.Name, this.x, this.y);
+            text(this.AlternateVariation, this.x, this.y + 5);
         } if (this.MovementType == "Armoured") {
-            textSize(5)
+            textSize(7);
             fill(255, 230, 90);
+            square(this.x, this.y - 10, 25, 5);
             text(this.Name, this.x, this.y);
+            text(this.AlternateVariation, this.x, this.y + 5);
         } 
+    }
+    summonHero(row) {
+        text(row.arr[0], width / 2, height / 2.2);
     }
 }
